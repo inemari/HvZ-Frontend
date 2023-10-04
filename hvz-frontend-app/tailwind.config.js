9f3cce428699c3a93138d9492eb6bf433905e902
Ine Mari Bredesen <inemari.bredesen@no.experis.com>
2023-10-04 10:35:19 +0200

    SCRUM-110 Setup project

---
+++ hvz-frontend-app/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        customOrange: '#AE5C2E',
+        customGreen: '#525437',
+        customWhite: '#FFFFFF',
+        customLightOrange: 'hsl(25, 19%, 86%)',
+        customBrown: '#453435',
+        customDarkOrange: '#AE5C2E',
+      },
+    },
+  },
+  plugins: [
+    // Other plugins if needed
+  ],
+};
