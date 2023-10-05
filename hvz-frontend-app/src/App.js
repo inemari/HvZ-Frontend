5e3142739c8f9737d3bd6a741f310f88c5d9760a
Tobias Vetrhus <tobias-vetrhus@outlook.com>
2023-10-05 13:53:45 +0200

    Updated app.js with keycloak

--- hvz-frontend-app/src/App.js
+++ hvz-frontend-app/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/custom.css'; // Import your custom CSS
 import LandingPage from './views/LandingPage';
+import { initialize } from './keycloak';
 
 const App = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    // Initialize Keycloak when the component mounts
+    initialize()
+      .then((authenticated) => {
+        setAuthenticated(authenticated);
+      })
+      .catch((error) => {
+        console.error('Keycloak initialization error', error);
+      });
+    }, []);
+
   return (
     <div className="App relative bg-black">
       <div className="background-image absolute top-0 left-0 w-full h-full "></div>
