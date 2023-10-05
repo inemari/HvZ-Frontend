4a4603f02af63ab95d48b569d987cb0a65a643fd
Ine Mari Bredesen <inemari.bredesen@no.experis.com>
2023-10-05 15:37:16 +0200

    Commented out keycloak

--- hvz-frontend-app/src/App.js
+++ hvz-frontend-app/src/App.js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './styles/custom.css'; // Import your custom CSS
 import LandingPage from './views/LandingPage';
-import { initialize } from './keycloak';
+// import { initialize } from './keycloak';
 
 const App = () => {
-  const [authenticated, setAuthenticated] = useState(false);
+  // const [authenticated, setAuthenticated] = useState(false);
 
-  useEffect(() => {
-    // Initialize Keycloak when the component mounts
-    initialize()
-      .then((authenticated) => {
-        setAuthenticated(authenticated);
-      })
-      .catch((error) => {
-        console.error('Keycloak initialization error', error);
-      });
-    }, []);
+  // useEffect(() => {
+  //   // Initialize Keycloak when the component mounts
+  //   initialize()
+  //     .then((authenticated) => {
+  //       setAuthenticated(authenticated);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Keycloak initialization error', error);
+  //     });
+  // }, []);
 
   return (
     <div className="relative">
-    <div className="dark-bg absolute"></div>
-    <div className="background-image absolute top-0 left-0"></div>
-    <LandingPage />
-  </div>
+      <div className="dark-bg absolute"></div>
+      <div className="background-image absolute top-0 left-0"></div>
+      <LandingPage />
+    </div>
   );
 };
 
