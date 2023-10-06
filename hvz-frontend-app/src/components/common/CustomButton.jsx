60d21f08712d8b03660c2932538ff0f767befddc
Ine Mari Bredesen <inemari.bredesen@no.experis.com>
2023-10-06 08:27:48 +0200

    Tried to recover original structure after mishaps

--- hvz-frontend-app/src/components/common/CustomButton.jsx
+++ hvz-frontend-app/src/components/common/CustomButton.jsx
@@ -2,17 +2,17 @@
 
 import React from 'react';
 
-const Button = ({ label, onClick }) => {
+const CustomButton = ({ label, onClick }) => {
 
 
   return (
     <button
       onClick={onClick}
-      className={`flex box-border md:max-w- w-full justify-center font-bold h-fit py-2 rounded-full bg-customOrange hover:bg-opacity-90 text-customWhite  md:px-5 md:text-lg  lg:px-6 lg:text-xl`}
+      className={`flex box-border md:max-w-xs w-full justify-center font-bold h-fit py-2 rounded-full bg-customOrange hover:bg-opacity-90 text-customWhite  md:px-5 md:text-lg  lg:px-6 lg:text-xl`}
     >
       {label}
     </button>
   );
 };
 
-export default Button;
+export default CustomButton;
