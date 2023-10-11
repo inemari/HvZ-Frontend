a5f0bc77d8fe822ede2ac0edec49bc345adf6f47
Noah Brovig Høgstøl <noah.brovig.hogstol@no.experis.com>
2023-10-11 13:44:44 +0200

    added updated version of Game.jsx in 'dev'

--- src/views/Game.jsx
+++ src/views/Game.jsx
@@ -2,17 +2,11 @@ import React from 'react';
 import GameMap from '../components/map/GameMap'; // Adjust the import path
 
 function Game() {
-<<<<<<< HEAD
-    return (
-        <h1 className='text-5xl justify-center'>GAMEPAGE</h1>
-    )
-=======
   return (
     <div className="mt-4 mx-auto">
       <GameMap />
     </div>
   );
->>>>>>> SCRUM-146-interactive-map
 }
 
 export default Game;
