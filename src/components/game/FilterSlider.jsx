6341e046e8548d403dc5f22b3d69177d080beb45
Ine Mari Bredesen <inemari.bredesen@no.experis.com>
2023-10-12 09:15:26 +0200

    added tommys filteradjustments

--- src/components/game/FilterSlider.jsx
+++ src/components/game/FilterSlider.jsx
@@ -1,40 +1,39 @@
 // FilterSlider.jsx
 
 import React, { useEffect, useState } from 'react';
-import { fetchAllGameStates, getGames } from '../../services/gameService';
+import { getGameStatusValues } from '../../services/gameService';
 
 const FilterSlider = ({ activeTab, handleTabChange }) => {
   const [filterOptions, setFilterOptions] = useState([]);
 
   useEffect(() => {
-    async function fetchGameStates() {
+    async function fetchGameStatusValues() {
       try {
-        // Use the service function to fetch game status values
-        const gameStatusValues = await fetchAllGameStates();
+        const gameStatusValues = await getGameStatusValues();
         setFilterOptions(gameStatusValues);
-        console.log(gameStatusValues);
       } catch (error) {
         console.error('There has been a problem with fetching game status values:', error);
       }
     }
-    fetchGameStates();
-  }, []); // Empty dependency array means this effect runs once on component mount
+    fetchGameStatusValues();
+  }, []);
 
   return (
     <div className="container mx-auto bg-customLightBrown flex rounded-md">
       {filterOptions.map((option) => (
-        <React.Fragment key={option}>
+        <React.Fragment key={option.gameState}>
           <button
-            onClick={() => handleTabChange(option)}
-            className={`${activeTab === option
+            onClick={() => handleTabChange(option.gameState)}
+            className={`${activeTab === option.gameState
               ? 'bg-customBrown text-white rounded-md '
               : ' text-customWhite hover:bg-customBrown hover:bg-opacity-80 hover:rounded-md '
               } py-2 px-4 flex-1 text-center relative`}
           >
-            {option}
+            {option.gameStateString}
           </button>
         </React.Fragment>
       ))}
+
     </div>
   );
 };
--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,6 +1,18 @@
 // gameService.js
 
-import { fetchGames, fetchGamesByState } from './api';
+import { fetchGames, fetchGamesByState } from "./api";
+
+function removeDuplicates(arr, prop) {
+    const unique = new Set();
+    return arr.filter((item) => {
+        const value = item[prop];
+        if (unique.has(value)) {
+            return false; // Duplicate, exclude it
+        }
+        unique.add(value);
+        return true; // Not a duplicate, include it
+    });
+}
 
 export const getGames = async () => {
     try {
@@ -11,23 +23,28 @@ export const getGames = async () => {
     }
 };
 
-export const getGamesByState = async (state) => {
+export const getGamesByState = async (gameState) => {
     try {
-        const gamesData = await fetchGamesByState(state);
+        const gamesData = await fetchGamesByState(gameState);
         return gamesData;
     } catch (error) {
         throw error;
     }
 };
 
-export const fetchAllGameStates = async () => {
+export const getGameStatusValues = async () => {
     try {
         const gamesData = await fetchGames();
-        // Extract game status values from the response
-        const gameStatusValues = gamesData.map(game => game.gameStateString);
+        // Extract game status values (both gameStateString and gameState) from the response
+        const gameStatusValues = gamesData.map((game) => ({
+            gameStateString: game.gameStateString,
+            gameState: game.gameState,
+        }));
         // Remove duplicate values and return them
-        return [...new Set(gameStatusValues)];
+        const uniqueGameStatusValues = removeDuplicates(gameStatusValues, 'gameState');
+        return uniqueGameStatusValues
     } catch (error) {
         throw error;
     }
 };
+
