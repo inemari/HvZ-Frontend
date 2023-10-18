e2c2429de002021b4dd67bee4c68f1ba4cff4b49
Tommy <tommyjaa@hotmail.com>
2023-10-18 14:54:39 +0200

    Map does now show mission markers again

--- src/components/missions/MissionMarker.jsx
+++ src/components/missions/MissionMarker.jsx
@@ -1,42 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import { getCoordinatesByMissionId } from '../../services/useMissionAndLocation';
+import React, { useState, useEffect } from "react";
+import { getMission, getLocation } from "../../services/mapService";
 
 const MissionMarker = ({ missionId }) => {
-    const [isPopOverVisible, setIsPopOverVisible] = useState(false);
-    const [location, setLocation] = useState(null);
+  const [missionData, setMissionData] = useState(null);
+  const [isPopOverVisible, setIsPopOverVisible] = useState(false);
+  const [x, setX] = useState(null);
+  const [y, setY] = useState(null);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const mission = await getMission(missionId);
+        const location = await getLocation(mission.locationId);
 
-    const handleMarkerClick = (e) => {
-        e.preventDefault();
-        setIsPopOverVisible((prevState) => !prevState);
-    };
+        // Extract x and y coordinates from the location object
+        const xCoordinate = location.xCoordinate;
+        const yCoordinate = location.yCoordinate;
 
-    if (!location) return null;
+        // Set the x and y coordinates in the state
+        setX(xCoordinate);
+        setY(yCoordinate);
 
-    return (
+        mission.location = location;
+        setMissionData(mission);
+      } catch (error) {
+        console.error("Failed to fetch mission or location:", error);
+      }
+    }
+
+    fetchData();
+  }, [missionId]);
+
+  const handleMarkerClick = (e) => {
+    e.preventDefault();
+    setIsPopOverVisible(!isPopOverVisible); // Toggle the popover visibility
+  };
+
+  return (
+    <div>
+      {x !== null && y !== null && (
         <div
-            className="absolute bg-red-500 text-white aspect-square text-center flex flex-col justify-center p-1 rounded-full cursor-pointer transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            style={{
-                left: `${getCoordinatesByMissionId(missionId).xCoordinate}%`,
-                top: `${getCoordinatesByMissionId(missionId).yCoordinate}%`,
-            }}
-            onClick={handleMarkerClick}
+          className="absolute bg-red-500 text-white aspect-square text-center flex flex-col justify-center p-1 rounded-full cursor-pointer transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          data-ripple-light="true"
+          style={{
+            left: `${x}%`,
+            top: `${y}%`,
+          }}
+          onClick={handleMarkerClick}
         >
-            M{missionId}
-            {isPopOverVisible && (
-                <div
-                    className="text-black absolute whitespace-normal bg-white border border-gray-300 rounded-lg shadow-lg p-3"
-                    style={{
-                        left: `${location.xCoordinate}px`,
-                        top: `${location.yCoordinate + 30}px`,
-                    }}
-                >
-                    <h3 className="font-semibold text-gray-900">{location.name}</h3>
-                    <p>{location.description}</p>
-                </div>
-            )}
+          M{missionId}
+          {isPopOverVisible && missionData && (
+            <div
+              className="text-black absolute whitespace-normal bg-white  border-gray-200  dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800 break-words rounded-lg border font-sans text-sm font-normal text-blue-gray-500 shadow-lg shadow-blue-gray-500/10 focus:outline-none"
+              style={{
+                left: `${x}px`, // Adjust the popover position if needed
+                top: `${y + 30}px`, // Adjust the popover position if needed
+              }}
+            >
+              <div className="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                <h3 className="font-semibold text-gray-900 dark:text-white">
+                  {" "}
+                  {missionData.name}
+                </h3>
+              </div>
+              <div className="px-3 py-2">
+                <p>{missionData.description}</p>
+              </div>
+            </div>
+          )}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default MissionMarker;
