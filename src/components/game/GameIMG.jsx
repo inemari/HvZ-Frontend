aa3e80a130ee56b8b7fa79854076cf236fcb51ec
Ine Mari Bredesen <inemari.bredesen@no.experis.com>
2023-10-20 10:18:06 +0200

    implemented default photo for image errors

--- src/components/game/GameIMG.jsx
+++ src/components/game/GameIMG.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import noImage from '../../assets/ui/noImage.png';
 
 const GameIMG = ({ game }) => {
-
     return (
-        <img className="rounded aspect-square m-auto" src={game.pictureURL} alt={game.title} />
+        <img
+            className="rounded aspect-square m-auto"
+            src={game.pictureURL}
+            alt={game.title}
+            onError={(e) => {
+                e.target.src = noImage;
+            }}
+        />
     );
 };
 
